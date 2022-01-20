58892ff9f3ea09cd54b0d80e23983d384ebb418f
jitendra <g203198@gmail.com>
2022-01-20 19:14:02 +0530

    configer all static

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 # Home page function
 def Home(request):
     return render(request,'pages/home.html')
+
+def About(request):
+    return render(request,'pages/about.html')
+
+def Servecs(request):
+    return render(request,'pages/servec.html')
+
+def Contact(request):
+    return render(request,'pages/contact.html')
