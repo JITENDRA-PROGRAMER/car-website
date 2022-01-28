af505ec17422f28971e5d3a57ef77ade247e255f
jitendra <g203198@gmail.com>
2022-01-28 06:46:01 +0530

    cutem database

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "carzone",
+        'USER': "postgres",
+        'PASSWORD': "jitu",
+        'HOST': "localhost",
+        'PORT': "5432",  # 5432 by default
     }
 }
 
@@ -113,7 +117,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -124,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,26 @@
+import imghdr
 from django.shortcuts import render
 
+from pages.models import Team
+from .models import *
 # Create your views here.
 
 # Home page function
 def Home(request):
-    return render(request,'pages/home.html')
+    teams = Team.objects.all()
+    data = {
+        'teams':teams,
+    }
+    return render(request,'pages/home.html',data)
+
+
 
 def About(request):
-    return render(request,'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams':teams,
+    }
+    return render(request,'pages/about.html',data)
 
 def Servecs(request):
     return render(request,'pages/servec.html')
