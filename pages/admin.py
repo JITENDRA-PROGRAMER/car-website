64bdd33f5859080fd30b4369508fef823f79ac03
jitendra <g203198@gmail.com>
2022-01-28 06:50:39 +0530

    createing tema models

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Team
 from django.utils.html import format_html
 # Register your models here.
+
+# Make coustem database designing 
 class TeamAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="40" style="border-radius: 50px;"  />'.format(object.photo.url))
@@ -13,4 +15,6 @@ class TeamAdmin(admin.ModelAdmin):
     list_display_links = ('id','thumbnail','firstname')
     search_fields = ('firstname','lastname','designation')
     list_filter = ('designation',)
-admin.site.register(Team,TeamAdmin)
+
+
+admin.site.register(Team,TeamAdmin) #Register model
